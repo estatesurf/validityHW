7bbee0166ec75723c186b6dcf5d2fb8be7230c47
estatesurf <estatesurfrentals@gmail.com>
2019-09-20 14:12:04 -0600

    add working processFile test

--- src/test/java/com/mlindner/AppTest.java
+++ src/test/java/com/mlindner/AppTest.java
@@ -2,10 +2,13 @@ package com.mlindner;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.io.*;
 
 public class AppTest {
 
     private static final String ROOT_PATH = "test";
+    public static String EMAIL_WORKING_DIR = "emailsDir";
+    public static String EXPECTED_ARCHIVE_STRUCTURE = "\\sampleEmails\\smallset";
 
     @Test
     public void iterateOverFiles() {
@@ -13,6 +16,16 @@ public class AppTest {
 
     @Test
     public void processFile() {
+        App app = new App();
+        BufferedReader br = null;
+        String filenameSTR = EMAIL_WORKING_DIR + EXPECTED_ARCHIVE_STRUCTURE + "\\20110401_aamarketinginc_14456749_html.msg"; 
+        try {
+            System.out.println(filenameSTR);
+            br = new BufferedReader(new FileReader(filenameSTR));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("survey@mindspaymails.com |  | Thu, 31 Mar 2011 23:19:52 -0500 ", app.processFile(br));
     }
 
     @Test
